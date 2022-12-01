74d2dfbc3cae2f2e9be20d09f10b5b7d2dd88611
maxim <maxim@LAMO>
2022-12-01 18:12:19 +0200

    Adding PyQt5 test

--- test.py
+++ test.py
@@ -1,1 +1,18 @@
-12
+from PyQt5.QtWidgets import *
+from PyQt5 import uic
+
+class MyGUI(QMainWindow):
+    def __init__(self):
+        super(MyGUI, self).__init__()
+        uic.loadUi("test.ui",self) # file
+        self.show()
+
+def main():
+    app = QApplication([])
+    window = MyGUI()
+    app.exec_()
+
+if __name__ == '__main__':
+    main()
+
+
