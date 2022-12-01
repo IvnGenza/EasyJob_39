374881ddebd233603ec0a113a9bed4e128abbda6
IvnGenza <you@example.com>
2022-12-01 19:14:32 +0200

    main

---
+++ main.py
@@ -0,0 +1,14 @@
+import pyrebase
+
+firebaseConfig ={
+'apiKey': "AIzaSyAwzsiCVmWkqAwh_zDR1XDsFbl_1g3vicw",
+'authDomain': "dj-test-ac00d.firebaseapp.com",
+'databaseURL': "https://dj-test-ac00d-default-rtdb.asia-southeast1.firebasedatabase.app",
+'projectId': "dj-test-ac00d",
+'storageBucket': "dj-test-ac00d.appspot.com",
+'messagingSenderId': "324559082205",
+'appId': "1:324559082205:web:bc45a327ebcce2a30b0134",
+'measurementId': "G-4FS7N8MX70"
+};
+
+firebase=pyrebase.initialize_app(firebaseConfig)
