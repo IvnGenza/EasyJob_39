5797a7e9daa9af89bf5e5c2aa35892b158ee99a2
maxim <maxim@LAMO>
2022-12-02 15:12:52 +0200

    adding database and logic folders, and moved the databace test to the database folder

--- main.py
+++ main.py
@@ -1,25 +1,3 @@
-import pyrebase
-
-# Test for adding a database
-firebaseConfig ={
-'apiKey': "AIzaSyAwzsiCVmWkqAwh_zDR1XDsFbl_1g3vicw",
-'authDomain': "dj-test-ac00d.firebaseapp.com",
-'databaseURL': "https://dj-test-ac00d-default-rtdb.asia-southeast1.firebasedatabase.app",
-'projectId': "dj-test-ac00d",
-'storageBucket': "dj-test-ac00d.appspot.com",
-'messagingSenderId': "324559082205",
-'appId': "1:324559082205:web:bc45a327ebcce2a30b0134",
-'measurementId': "G-4FS7N8MX70"
-};
-
-firebase=pyrebase.initialize_app(firebaseConfig)
-db=firebase.database()
-
-p=db.child("Data").child("Name").get().val()
-
-print(p)
-
-
 
 # Test for creating a window with PyQt5
 from PyQt5.QtWidgets import *
@@ -28,7 +6,7 @@ from PyQt5 import uic
 class MyGUI(QMainWindow):
     def __init__(self):
         super(MyGUI, self).__init__()
-        uic.loadUi("Ui_Files/test.ui", self) # file
+        uic.loadUi("ui/test.ui", self) # file
         self.show()
 
 def main():
