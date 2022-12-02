7ebe728db0aa4911361c265e9536e2b435725a76
maxim <maxim@LAMO>
2022-12-02 19:00:02 +0200

    added the functionality of changing windows

--- main.py
+++ main.py
@@ -1,18 +1,62 @@
 
 # Test for creating a window with PyQt5
-from PyQt5.QtWidgets import *
-from PyQt5 import uic
+import sys
+from PyQt5.uic import loadUi
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QApplication, QMainWindow
 
-class MyGUI(QMainWindow):
+
+class Signup(QMainWindow):
     def __init__(self):
-        super(MyGUI, self).__init__()
-        uic.loadUi("ui/signup.ui", self) # file
-        self.show()
+        super(Signup, self).__init__()
+        loadUi("ui/signup.ui", self) # file
+        self.handle_buttons() # allows us to listen for clicks on all the buttons
 
-def main():
-    app = QApplication([])
-    window = MyGUI()
-    app.exec_()
+    def printTest(self): # just a test function
+        print('this is a test\n')
 
-if __name__ == '__main__':
-    main()
+    def change(self): # just a test function
+        test = Test()
+        widget.addWidget(test)
+        widget.setCurrentIndex(widget.currentIndex()+1) # -1 also works?
+
+    def handle_buttons(self): # this function handles the click of the signup button
+        self.sign_up_button.clicked.connect(self.printTest)
+        self.existing_account_button.clicked.connect(self.change)
+
+
+class Test(QMainWindow):
+    def __init__(self):
+        super(Test, self).__init__()
+        loadUi("ui/test.ui", self) # file
+        self.handle_buttons() # allows us to listen for clicks on the signup button
+
+    def change(self): # just a test function
+        signup = Signup()
+        widget.addWidget(signup)
+        widget.setCurrentIndex(widget.currentIndex()+1)
+
+    def handle_buttons(self): # this function handles the click of the signup button
+        self.pushButton_5.clicked.connect(self.change)
+
+
+
+
+app = QApplication(sys.argv)
+widget = QtWidgets.QStackedWidget() # creates a Stack of widgets(windows)
+
+test = Test()
+widget.addWidget(test) # adding the first window to the stack
+
+widget.setFixedHeight(900)
+widget.setFixedWidth(1500)
+widget.show() # showing the stack of widgets, first window will be showen first
+
+try:
+    sys.exit(app.exec_()) # tring to run the app
+except:
+    print("Exiting")
+
+
+#login = Login()
+#widget.addWidget(login)
