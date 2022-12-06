50ff9a24b02dc7d7c947c1ae37c11031d33dd2d1
IvnGenza <you@example.com>
2022-12-06 14:07:20 +0200

    SignUp,Login funcs

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-
-# Test for creating a window with PyQt5
+import database.authentication
 import sys
 from PyQt5.uic import loadUi
 from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QApplication, QMainWindow
-
 
 class Signup(QMainWindow):
     def __init__(self):
@@ -12,17 +10,88 @@ class Signup(QMainWindow):
         loadUi("ui/signup.ui", self) # file
         self.handle_buttons() # allows us to listen for clicks on all the buttons
 
-    def printTest(self): # just a test function
-        print('this is a test\n')
 
-    def change(self): # just a test function
+
+    def CreateNewAccFunc(self):
+        count=0
+
+        ##confirm
+
+        if self.checkEmail() :
+            email = self.Email_text_box.text()
+            count+=1
+
+
+        if self.checkPasswordKey():
+            count+=1
+            PasswordKey=self.password_text_box.text()
+
+
+        if self.checkFullName():
+            count+=1
+            FullName=self.full_name_text_box.text()
+
+
+        if self.checkAge():
+            count+=1
+            Age=self.age_text_box.text()
+
+
+        if self.checkUserName():
+            count+=1
+            serName=self.username_text_box.text()
+
+
+        if self.checkPerson():
+            count+=1
+            UserType=self.user_type_text_box.text()
+
+
+        if count==6 :
+
+            try:
+
+                self.change_login()
+                database.authentication.auth.create_user_with_email_and_password(email,PasswordKey)
+
+            except:
+                self.wrong_data_label.setVisible(True)
+
+
+# help func`s for signup class.
+
+
+    def checkPasswordKey(passkey):
+        return True
+
+    def checkEmail(email):
+        return True
+
+    def checkAge(age):
+        return True
+
+    def checkFullName(fullname):
+        return True
+
+    def checkUserName(username):
+        return True
+
+    def checkePerson(person):
+        return True
+
+
+    def change_to_login(self): # just a test function
         login = Login()
         widget.addWidget(login)
         widget.setCurrentIndex(widget.currentIndex()+1) # -1 also works?
 
     def handle_buttons(self): # this function handles the click of the signup button
-        self.sign_up_button.clicked.connect(self.printTest)
-        self.existing_account_button.clicked.connect(self.change)
+        self.sign_up_button.clicked.connect(self.CreateNewAccFunc)
+        self.existing_account_button.clicked.connect(self.change_to_login)
+        self.wrong_data_label.setVisible(False)
+        self.wrong_input.setVisible(False)
+
+#------------------------------------help func`s------------------------------------
 
 
 class Login(QMainWindow):
@@ -31,15 +100,44 @@ class Login(QMainWindow):
         loadUi("ui/login.ui", self) # file
         self.handle_buttons() # allows us to listen for clicks on the signup button
 
-    def change(self): # just a test function
+    def logging(self):
+
+        if checkPasswordKey() and checkEmail:
+
+            email=self.username_lable.text()
+            passwordKey=self.password_lable.text()
+
+        try:
+            database.authentication.auth.sign_in_with_email_and_password(email,passwordKey)
+            print(">> Welcome! <<")
+
+        except:
+            self.wrong_input.setVisible(True)
+
+
+
+
+
+
+
+#       help func`s for login class.
+
+        def checkPasswordKey(passkey):
+            return True
+
+        def checkEmail(email):
+            return True
+
+
+    def change_to_signup(self): # just a test function
         signup = Signup()
         widget.addWidget(signup)
         widget.setCurrentIndex(widget.currentIndex()+1)
 
     def handle_buttons(self): # this function handles the click of the signup button
-        self.sign_up_button.clicked.connect(self.change)
+        self.sign_up_button.clicked.connect(self.change_to_signup)
 
-
+#--------------------------------help func`s--------------------------------
 
 
 app = QApplication(sys.argv)
