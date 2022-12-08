bf25038d666034eb7b689d4cf05876ef919f6b70
Maxim Subotin <maximsu@ac.sce.ac.il>
2022-12-08 19:50:50 +0200

    adding the feature to add jobs to the database in the auth file and well as fixing stuff in the homepage.ui file

--- database/authentication.py
+++ database/authentication.py
@@ -26,3 +26,31 @@ SomeUser = db.child("Users").order_by_child('email').equal_to('name@name.name').
 
 
 print(SomeUser)
+
+
+#pushing some job adds to the database, you can use these lines, 
+#just change up the information and run THIS file. 
+#or you can take these lines elsewhere and run there. 
+#adding stuff this way allows for randomly generated keys for the database for added security
+
+##this is the data you want to push to the database, is something went wrong you can go into the database URL and change stuff there btw
+#data={
+#    "title": "Microsoft Needs A System Analyst",
+#    "description": "bla bla bla this is some text", 
+#    "contactInfo": ["Lisa Doe", "0509988556", "email5@gmail.com"], 
+#    "knowledge": ["C++","Java","PHP"],
+#    "preferences": {
+#        "workExperience": "more then 5 years",
+#        "daysPerWeek": "more then 3 days", 
+#        "workingFrom": "hybrid"
+#        },
+#    "search": {
+#        "role": "Business systems analyst", 
+#        "location": "Tel Aviv-Yafo", 
+#        "degree": "Ph.D (3nd)", 
+#        "jobType": "Full Time"
+#        }
+#}
+
+##this addes the above data into the 'Jobs' section of the realtime database
+#db.child('Jobs').push(data)
