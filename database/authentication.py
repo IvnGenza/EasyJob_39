5e2c6a25e90e4237f4ca969065c68fd52f8ff57f
Maxim Subotin <maximsu@ac.sce.ac.il>
2022-12-12 11:19:24 +0200

    updating auth.py file

--- database/authentication.py
+++ database/authentication.py
@@ -53,16 +53,7 @@ print(SomeUser)
 #}
 
 
-#data= [{
-#        "username": "Max",
-#        "resume": "this is my resume, bla bla bla this is some text", 
-#        "email":"Max@Max.com"
-#},{
-#        "username": "Shay",
-#        "resume": "this is my resume, bla bla bla this is some text", 
-#        "email":"Shay@Shay.com"
-#},
-#]    
+
 
 
 
